af26aad5113e3db163ab933afc88df3662408631
Alexander <alethogar@protonmail.com>
2026-01-24 03:44:36 +0100

    Fixed experimental test of new Flag

--- tests/ExampleAdvanced.py
+++ tests/ExampleAdvanced.py
@@ -26,7 +26,7 @@ class Main:
             useDefaultValue= True, defaultValue= [],
             description= "List of external vendors to update")
 
-        updateExternalFlag = BoolFlag("UpdateExternalVendors", ["updateexternal", "uev", "ue"], 
+        updateExternalFlag = Flag("UpdateExternalVendors", ["updateexternal", "uev", "ue"], 
             description= "Update all external vendors with new values.") 
 
         helpCommand = Command("Help", ["help", "h", "man"],
